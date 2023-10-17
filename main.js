6870ee1382897abc9186d61db90ef8a68f8f70c1
Butter3eer <palcsenge2015@gmail.com>
2023-10-17 11:02:34 +0200

    final touches

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 import './node_modules/bootstrap/dist/css/bootstrap.css';
 import './node_modules/bootstrap/dist/js/bootstrap.js';
+import './style.css';
 
 document.addEventListener('DOMContentLoaded', init);
 
